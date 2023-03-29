4f9765024103c4f560f53f0e30769a02edc8538d
Mohammed Shahid Adoni <77674447+shahidadoni@users.noreply.github.com>
2023-03-29 18:10:29 +0530

    First Commit

--- backend/checkUserExists.py
+++ backend/checkUserExists.py
@@ -1,23 +1,28 @@
 import json
 import boto3
 import os
+import io
+import base64
 
 client = boto3.client('dynamodb')
 table_name = os.environ['TableName']
+rekognition = boto3.client('rekognition', region_name='us-west-1')
+dynamodb = boto3.client('dynamodb', region_name='us-west-1')
+faceTable = os.environ['RVMSFaceTable']
 
 def lambda_handler(event, context):
     # TODO implement
     
     print(event)
     
-    if "emailId" not in event and "phoneNumber" not in event:
+    if "emailId" not in event and "phoneNumber" not in event and "faceImage" not in event:
         
         return {
             'statusCode': 422,
             'headers': { "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "Content-Type",
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET" },
-            'body': {'message':'Please Enter Email-Id or Mobile Number !!!'}
+            'body': {'message':'Please Enter Email-Id, Mobile Number or upload a photo !!!'}
         }
     
     elif "emailId" in event:
@@ -96,4 +101,65 @@ def lambda_handler(event, context):
                     'name' : phoneNumber_response['Items'][0]['name']
                 }
             }
+    elif "faceImage" in event:
+        face_emailId = ""
+        photograph = event["faceImage"]
+        photograph = photograph[photograph.find(",")+1:] 
+        imgdata = base64.b64decode(photograph + "===")
+        Body=io.BytesIO(imgdata)
+        image_binary = Body.getvalue()
+        response = rekognition.search_faces_by_image(
+                CollectionId='rvms_collection',
+                Image={'Bytes':image_binary}                                       
+                )
+            
+        for match in response['FaceMatches']:
+            print (match['Face']['FaceId'],match['Face']['Confidence'])
+                
+            face = dynamodb.get_item(
+                TableName=faceTable,  
+                Key={'RekognitionId': {'S': match['Face']['FaceId']}}
+                )
+            
+            if 'Item' in face:
+                face_emailId = face['Item']['FullName']['S']
+                # print (face_emailId)
+            else:
+                print ('no match found in person lookup')
+        print(face_emailId)  
+        face_response = client.query(
+            ExpressionAttributeValues={
+                ':v1': {
+                    'S': face_emailId,
+                },
+            },
+            KeyConditionExpression='emailId = :v1',
+            TableName=table_name,
+        )
+
         
+        item_length = len(face_response['Items'])
+        
+        if item_length == 0:
+            return{
+                'statusCode': 422,
+                'headers': {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                },
+                'body': {'message':'User Not Found'}
+            }
+        else:
+            return{
+                'statusCode': 200,
+                'headers': {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                },
+                'body': {'message': 'User Found',
+                    'emailId': face_response['Items'][0]['emailId'],
+                    'name' : face_response['Items'][0]['name']
+                }
+            }
